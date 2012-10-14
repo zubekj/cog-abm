a8a0e548abe4519a1d2b4b2b51274a99698aeb3e
Konrad Kurdej <kkonrad@i_wont_tell.com>
2012-10-14 15:34:48 +0200

    Pickle version and PEP8 in simulation

--- src/cog_abm/core/simulation.py
+++ src/cog_abm/core/simulation.py
@@ -10,16 +10,15 @@ from ..extras.tools import get_progressbar
 
 log = logging.getLogger('COG-ABM')
 
+PICKLE_PROTOCOL = cPickle.HIGHEST_PROTOCOL
+
 
 class Simulation(object):
     """
     This class defines what happens and when.
     """
 
-    global_environment = None
-    environments = {}
-
-    def __init__(self,  graph=None,  interaction=None,  agents=None, pb=False):
+    def __init__(self, graph=None, interaction=None, agents=None, pb=False):
         ''' pb - show progress bar
         '''
         self.graph = graph
@@ -29,34 +28,29 @@ class Simulation(object):
         self.dump_often = False
         self.pb = pb
 
-
     def dump_results(self, iter_num):
         cc = copy.deepcopy(self.agents)
         #cc = [a.deepcopy() for a in self.agents]
         kr = (iter_num, cc)
         self.statistic.append(kr)
         if self.dump_often:
-            f = open(str(iter_num)+".pout", "wb")
-            cPickle.dump(kr, f)
+            f = open(str(iter_num) + ".pout", "wb")
+            cPickle.dump(kr, f, PICKLE_PROTOCOL)
             f.close()
-
 
     def _choose_agents(self):
         if self.interaction.num_agents() == 2:
-            l = random.randint(0, len(self.agents)-1)
-            a = self.agents[l]
+            a = random.choice(self.agents)
             b = self.graph.get_random_neighbour(a)
-            return [a,b]
-        else :
+            return [a, b]
+        else:
             return [random.choice(self.agents)]
-
 
     def _start_interaction(self, agents):
         self.interaction.interact(*agents)
 #               results = self.interaction.interact(*agents)
 #               for r, a in izip(results, agents):
 #                       a.add_inter_result(r)
-
 
     def _do_iterations(self, num_iter):
         for _ in xrange(num_iter):
@@ -66,20 +60,18 @@ class Simulation(object):
     def _do_main_loop(self, iterations, dump_freq):
         start_time = time()
         log.info("Simulation start...")
-        it = xrange(iterations//dump_freq)
+        it = xrange(iterations // dump_freq)
         if self.pb:
             it = get_progressbar()(it)
         for i in it:
             self._do_iterations(dump_freq)
-            self.dump_results((i+1)*dump_freq)
+            self.dump_results((i + 1) * dump_freq)
 
-        log.info("Simulation end. Total time: "+str(time()-start_time))
-
+        log.info("Simulation end. Total time: " + str(time() - start_time))
 
     def continue_(self, iterations=1000, dump_freq=10):
         self._do_main_loop(iterations, dump_freq)
         return self.statistic
-
 
     def run(self, iterations=1000, dump_freq=10):
         """
