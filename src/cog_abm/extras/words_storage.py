8d442d2a42cd3aefd93b27e29d1c6bab4f868d36
mlukasik <michal.mateusz.lukasik@gmail.com>
2012-12-29 21:00:16 +0100

    extract_colour_order.py as seperate file from words_storage.py and tests for it.

--- src/cog_abm/extras/words_storage.py
+++ src/cog_abm/extras/words_storage.py
@@ -5,65 +5,6 @@ Created on Dec 29, 2012
 
 @author: mlukasik
 '''
-
-#======================EXTRACT COLOUR ORDER======================#
-def extract_colour_order(environment, clab_name):
-    '''
-    Extract colour order from clab_fname and return stimuli in that order (exclude repetitions).
-    '''
-    chip_map = extract_chip_map(clab_name)
-    return get_environment_in_order(environment, chip_map)
-    
-def get_environment_in_order(environment, chip_map):
-    '''Return sorted list of stimuli from environment in order specified by chip_map.
-    '''
-    visited = set()
-    colour_order_keyed = []
-    #traverse all stimuli and add them to the list, excluding repetitions
-    for s in environment.stimuli:
-        (L, a, b) = tuple(s.get_values())
-        if (L, a, b) not in visited:
-            visited.add((L, a, b))
-            try:
-                colour_name = chip_map[(float(L), float(a), float(b))]
-            except:
-                print "[get_environment_in_order]: chip_map: ", chip_map
-                import sys
-                sys.exit(1)
-            colour_order_keyed.append( (s, colour_name) )
-    
-    #sort by colour_name
-    colour_order_keyed.sort(key = lambda x: x[1])
-    #return only necessary part
-    return map(lambda x: x[0], colour_order_keyed)
-
-
-def extract_chip_map(fname):
-    """
-    Read a chipmap from fname, extract information about which ordinal number is
-    given to each of the Lab coordinates.
-    
-    """
-    chip_map = {}
-    ind = 0
-    to_ind = 0
-    with open(fname) as f:
-        #each line is read from a c-lab file
-        for l in f:
-            ll = l.split()
-            #if it is already in a chip_map then there is some repetition!
-            if (float(ll[6]), float(ll[7]), float(ll[8])) in chip_map:
-                print "[extract_colour_order] ERROR!", (float(ll[6]), float(ll[7]), float(ll[8])), "in chip_map !!!"
-                import sys
-                sys.exit(1)
-            else:#else everything is ok
-                chip_map[(float(ll[6]), float(ll[7]), float(ll[8]))] = ll[0]
-                ind+=1
-                to_ind = max(to_ind, int(ll[0]))
-    return chip_map
-    
- 
-#======================STORE WORDS======================#
 def store_words(agents, colour_order, out_file):
     """Stores words used by agents to a file.
     
---
+++ src/test/test_extract_colour_order.py
@@ -0,0 +1,44 @@
+'''
+
+Created on Dec 29, 2012
+
+@author: mlukasik
+'''
+import sys
+sys.path.append('../')
+import unittest
+from cog_abm.extras.extract_colour_order import *
+
+class TestGetEnvironmentInOrder(unittest.TestCase):
+
+    def test_semi_reversed_order(self):
+        
+        class Environment(object):
+            def __init__(self):
+                self.stimuli = []
+        
+        class Colour(object):
+            def __init__(self, val):
+                self.val = val
+            def get_values(self):
+                return self.val
+        
+        environment = Environment()
+        ind = 1
+        chip_map = {}
+        for L in xrange(5):
+            for a in xrange(5):
+                    environment.stimuli.append( Colour((L, a, 0)) )
+        
+        expected_sequence = []
+        for L in xrange(5):
+            for a in reversed(range(5)):
+                    chip_map[(L, a, 0)] = ind
+                    ind += 1
+                    expected_sequence.append( (L, a, 0) )
+        
+        colour_sequence = get_environment_in_order(environment, chip_map)
+        self.assertEqual(expected_sequence, map(lambda x: x.get_values(), colour_sequence))
+        
+if __name__ == '__main__':
+    unittest.main()
