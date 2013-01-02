5b39d24a4783fdce42a4a447fffa9bf1d14c4bee
mlukasik <michal.mateusz.lukasik@gmail.com>
2013-01-03 00:33:54 +0100

    Adding Simulation files with weighted stimuli.

---
+++ src/steels/call_simulations_munsell1269_weighted.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# argument 1: suffix nazwy symulacji, np a_larger
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+do
+	sh ./call_simulation_move_results.sh "../cog_abm/extras/wordanalysis/input_xmls/1269_munsell_chips_weighted_simulation_xmls/simulation_artificial_"$1".xml" "wcs_1269_"$1""$i
+done
+
