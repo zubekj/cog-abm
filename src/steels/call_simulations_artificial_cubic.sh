34a90c2c0596a1620aef7836239fc73dbe93498f
mlukasik <michal.mateusz.lukasik@gmail.com>
2013-01-03 15:16:34 +0100

    Changes in simulation files - parameters tuning.

--- src/steels/call_simulations_artificial_cubic.sh
+++ src/steels/call_simulations_artificial_cubic.sh
@@ -2,6 +2,6 @@
 # argument 1: suffix nazwy symulacji, np normal
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
-	sh call_simulation_move_results.sh "../cog_abm/extras/wordanalysis/input_xmls/synthetic_cube_data/simulation_cube_"$1".xml" "cube_"$1""$i
+	sh call_simulation_move_results.sh "../cog_abm/extras/wordanalysis/input_xmls/synthetic_cube_simulations/simulation_cube_"$1".xml" "cube_"$1""$i
 done
 
