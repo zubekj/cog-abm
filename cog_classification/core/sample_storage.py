99875a627e86e215e6749bbe5a914b7b906b854b
przemekw <pwroblewski@student.uw.edu.pl>
2015-07-22 16:26:07 +0200

    Added tests to sample_storage.

--- cog_classification/core/sample_storage.py
+++ cog_classification/core/sample_storage.py
@@ -41,12 +41,13 @@ class SampleStorage:
         Sample can be added to multiple classes.
         """
 
-        sample_weight = sample_weight or self.new_weight
+        if sample_weight is None:
+            sample_weight = self.new_weight
 
         if sample_class in self.classes:
             if true_class == self.classes[sample_class]["true_class"]:
                 if sample not in self.classes[sample_class]["samples"]:
-                    self.classes[sample_class]["samples"][sample] = sample_weight
+                    self.set_weight(sample, sample_class, sample_weight)
                 else:
                     # If sample has already existed in sample class, then we strengthen this class with sample.
                     self.increase_weights_in_class(sample, sample_class)
@@ -63,31 +64,24 @@ class SampleStorage:
 
     def create_new_class(self, sample_class, true_class):
         """
-        Creation of new class in sample storage.
+        Creation of new class.
         If sample class is given then we assert that this class is not in classes.
         Create new class returns correct name of sample class and sample class body (dictionary).
+        This method doesn't add this new created class to classes.
         """
 
         if sample_class is None:
             # We create new name for sample class.
 
             # First we finds the name that wasn't used.
-            while self.class_name in self.classes:
+            while "Class number: " + str(self.class_name) in self.classes:
                 self.class_name += 1
 
-            sample_class = self.class_name
+            sample_class = "Class number: " + str(self.class_name)
             self.class_name += 1
 
         new_class = {"true_class": true_class, "samples": {}}
         return sample_class, new_class
-
-    def increase_weights_in_class(self, sample, sample_class):
-        """ Increase the weights of samples in sample class according to their distance to sample. """
-
-        for class_sample, old_weight in self.get_class_samples(sample_class):
-            weight_change = self.beta * 0.1 ** (self.sigma * sample.distance(class_sample))
-            new_weight = min(old_weight + weight_change, self.max_weight)
-            self.set_weight(class_sample, sample_class, new_weight)
 
     def decrease_weights_in_class(self, target_class):
         """ Decrease weights of every sample in target class according to alpha value. """
@@ -101,13 +95,81 @@ class SampleStorage:
         for target_class in self.get_classes():
             self.decrease_weights_in_class(target_class)
 
+    def empty(self):
+        return len(self.classes) == 0
+
+    def export(self):
+        """
+        Returns list of list of all attributes of every sample
+        and second list of class in which sample is in sample storage.
+        """
+        data = []
+        decisions = []
+
+        for target_class in self.get_classes():
+            for sample, _ in self.get_class_samples(target_class):
+                data.append(sample.get_attributes())
+                decisions.append(self.get_true_class(target_class))
+
+        return data, decisions
+
+    def increase_weights_in_class(self, sample, sample_class):
+        """ Increase the weights of samples in sample class according to their distance to sample. """
+
+        for class_sample, old_weight in self.get_class_samples(sample_class):
+            weight_change = self.beta * 0.1 ** (self.sigma * sample.distance(class_sample))
+            new_weight = min(old_weight + weight_change, self.max_weight)
+            self.set_weight(class_sample, sample_class, new_weight)
+
+    def remove_class(self, target_class):
+        self.classes.pop(target_class)
+
+    def remove_sample_from_class(self, sample, target_class):
+        self.classes[target_class]["samples"].pop(sample)
+        if self.get_class_number(target_class) == 0:
+            self.remove_class(target_class)
+
+    def remove_weak_samples(self):
+        for target_class in self.get_classes():
+            self.remove_weak_samples_from_class(target_class)
+
+    def remove_weak_samples_from_class(self, target_class):
+        """
+        Removes all samples which weight is lower than forgetting threshold.
+        If all samples are removed, class is removed too.
+        """
+
+        to_remove = []
+        all_class_samples = self.get_class_samples(target_class)
+        for sample, weight in all_class_samples:
+            if weight < self.forgetting_threshold:
+                to_remove.append(sample)
+
+        if len(to_remove) == len(all_class_samples):
+            self.remove_class(target_class)
+        else:
+            for sample in to_remove:
+                self.remove_sample_from_class(sample, target_class)
+
+    def get_class_number(self, target_class):
+        return len(self.classes[target_class]["samples"])
+
     def get_class_samples(self, sample_class):
-        """ Returns list of pair (sample, weight). """
+        """ Returns list of pair (sample, weight) of given sample class. """
         samples = self.classes[sample_class]["samples"]
         return [(sample, samples[sample]) for sample in samples]
 
+    def get_class_samples_size(self, target_class):
+        return len(self.classes[target_class]["samples"])
+
     def get_classes(self):
         return self.classes.keys()
+
+    def get_classes_number(self):
+        return len(self.classes)
+
+    def get_true_class(self, target_class):
+        return self.classes[target_class]["true_class"]
 
     def set_weight(self, sample, sample_class, new_weight):
         """ Sets weight of given sample in given sample class to new weight. """
--- cog_classification/tests/test_sample_storage.py
+++ cog_classification/tests/test_sample_storage.py
@@ -1,1 +1,204 @@
+from nose.tools import assert_equals, raises
 
+from cog_classification.core.sample_storage import SampleStorage
+
+from itertools import izip_longest
+
+
+class SimpleSample:
+
+        def __init__(self, number):
+            self.number = number
+
+        def distance(self, number):
+            return abs(self.number - number.get_attributes()[0])
+
+        def get_attributes(self):
+            return [self.number]
+
+
+class TestSampleStorage:
+
+    def __init__(self):
+        self.test_storage = SampleStorage()
+
+    def assert_size(self, number):
+        assert_equals(self.test_storage.get_classes_number(), number)
+
+    def setup(self):
+        self.test_storage = SampleStorage()
+
+    def test_add_sample_not_empty_after_adding(self):
+        assert_equals(self.test_storage.empty(), True)
+        self.test_storage.add_sample(SimpleSample(1), 1)
+        assert_equals(self.test_storage.empty(), False)
+
+    def test_add_sample_classes_number(self):
+        self.assert_size(0)
+
+        # Adding new element to non existing class.
+        self.test_storage.add_sample(SimpleSample(1), 1, 1)
+        self.assert_size(1)
+
+        # Adding old element to existing class.
+        self.test_storage.add_sample(SimpleSample(1), 1, 1)
+        self.assert_size(1)
+
+        # Adding new element to existing class.
+        self.test_storage.add_sample(SimpleSample(2), 1, 1)
+        self.assert_size(1)
+
+        # Adding new element to non existing class.
+        self.test_storage.add_sample(SimpleSample(3), 1, 2)
+        self.assert_size(2)
+
+        # Adding new element to existing class.
+        self.test_storage.add_sample(SimpleSample(3), 1, 1)
+        self.assert_size(2)
+
+        # Adding new element to non existing class.
+        self.test_storage.add_sample(SimpleSample(4), 1, 3)
+        self.assert_size(3)
+
+        # Adding old element to existing class (different true classes).
+        self.test_storage.add_sample(SimpleSample(4), 2, 3)
+        self.assert_size(4)
+
+        # Adding new element to existing class (different true classes).
+        self.test_storage.add_sample(SimpleSample(5), 2, 1)
+        self.assert_size(5)
+
+        # Adding new element without class.
+        self.test_storage.add_sample(SimpleSample(5), 2)
+        self.assert_size(6)
+
+    def test_decrease_weights_in_class_lowers_values(self):
+        self.test_storage.add_sample(SimpleSample(1), 1, 1, 1)
+        self.test_storage.add_sample(SimpleSample(2), 1, 1, 1)
+
+        for _, weight in self.test_storage.get_class_samples(1):
+            assert_equals(weight, 1)
+
+        self.test_storage.decrease_weights_in_class(1)
+
+        for _, weight in self.test_storage.get_class_samples(1):
+            assert weight < 1
+
+    def test_decrease_weights_in_class_no_under_zero(self):
+        self.test_storage.add_sample(SimpleSample(1), 1, 1, 0.01)
+        self.test_storage.add_sample(SimpleSample(2), 1, 1, 0.01)
+
+        for _ in range(1000):
+            self.test_storage.decrease_weights_in_class(1)
+
+        for _, weight in self.test_storage.get_class_samples(1):
+            assert weight > 0
+
+    def test_export(self):
+        true_data = []
+        true_decisions = []
+
+        for j in range(4, 40, 5):
+            for i in range(j, j + 3):
+                self.test_storage.add_sample(SimpleSample(i), j)
+                true_data.append([i])
+                true_decisions.append(j)
+
+        data, decisions = self.test_storage.export()
+
+        # Checking whether for every value decision pair in data and decisions
+        # exist exactly same value pair in true data and true decisions.
+        for value, decision in izip_longest(data, decisions):
+            pair_find = False
+            for true_value, true_decision in izip_longest(true_data, true_decisions):
+                if value == true_value and decision == true_decision:
+                    pair_find = True
+                    break
+            assert pair_find
+
+        # Checking whether for every value, decision pair in true data and true decisions
+        # exist exactly same value, decision pair in data and decisions.
+        for true_value, true_decision in izip_longest(true_data, true_decisions):
+            pair_find = False
+            for value, decision in izip_longest(data, decisions):
+                if value == true_value and decision == true_decision:
+                    pair_find = True
+                    break
+            assert pair_find
+
+    def test_increase_weights_in_class_higher_values(self):
+        self.test_storage.add_sample(SimpleSample(1), 1, 1, 0)
+        self.test_storage.add_sample(SimpleSample(2), 1, 1, 0)
+
+        for _, weight in self.test_storage.get_class_samples(1):
+            assert_equals(weight, 0)
+
+        self.test_storage.increase_weights_in_class(SimpleSample(1), 1)
+
+        for _, weight in self.test_storage.get_class_samples(1):
+            assert weight > 0
+
+    def test_increase_weights_in_class_no_over_one(self):
+        self.test_storage.add_sample(SimpleSample(1), 1, 1, 1)
+        self.test_storage.add_sample(SimpleSample(2), 1, 1, 1)
+
+        for _, weight in self.test_storage.get_class_samples(1):
+            assert_equals(weight, 1)
+
+        self.test_storage.increase_weights_in_class(SimpleSample(1), 1)
+
+        for _, weight in self.test_storage.get_class_samples(1):
+            assert_equals(weight, 1)
+
+    @raises(KeyError)
+    def test_remove_class_from_empty_storage_fails(self):
+        self.test_storage.remove_class(1)
+
+    @raises(KeyError)
+    def test_remove_class_other_class_fails(self):
+        self.test_storage.add_sample(SimpleSample(1), 1, 1, 1)
+        self.test_storage.remove_class(2)
+
+    def test_remove_class_from_full_to_empty(self):
+        self.test_add_sample_classes_number()
+        classes = self.test_storage.get_classes()
+        classes_number = self.test_storage.get_classes_number()
+
+        for removed_class in classes:
+            self.assert_size(classes_number)
+            self.test_storage.remove_class(removed_class)
+            classes_number -= 1
+
+        self.assert_size(0)
+
+    def test_remove_class_add_class_and_remove_class(self):
+        self.assert_size(0)
+        self.test_storage.add_sample(SimpleSample(1), 1, 1, 1)
+        self.assert_size(1)
+        self.test_storage.remove_class(1)
+        self.assert_size(0)
+        self.test_storage.add_sample(SimpleSample(1), 1, 1, 1)
+        self.assert_size(1)
+        assert 1 in self.test_storage.get_classes()
+
+    @raises(KeyError)
+    def test_remove_sample_from_class_empty_storage_fails(self):
+        self.test_storage.remove_sample_from_class(1, 1)
+
+    @raises(KeyError)
+    def test_remove_sample_from_class_storage_without_sample_fails(self):
+        self.test_storage.add_sample(2, 1, 1)
+        self.test_storage.remove_sample_from_class(3, 1)
+
+    @raises(KeyError)
+    def test_remove_sample_from_class_other_class_fails(self):
+        self.test_storage.add_sample(1, 1, 1)
+        self.test_storage.remove_sample_from_class(1, 2)
+
+    def test_remove_sample_from_class_last_sample_removes_class(self):
+        self.test_storage.add_sample(1, 1, 1)
+        self.assert_size(1)
+        assert_equals(1, self.test_storage.get_class_samples_size(1))
+
+        self.test_storage.remove_sample_from_class(1, 1)
+        self.assert_size(0)
