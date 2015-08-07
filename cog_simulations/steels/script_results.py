b2b11bce611db0f44d33d50160ce7f396d4b8ef5
julekb <julek.baranski@gmail.com>
2015-08-07 12:39:07 +0200

    New temporary scripts for simulations

---
+++ cog_simulations/steels/script_results.py
@@ -0,0 +1,18 @@
+import os
+for i in xrange(20):
+ 
+ # os.system("python steels_main.py -s ../../examples/simulations/sim2.json -r  ../../results_of_simulation/results_proba"+format(i)),
+
+     
+#  os.system("python2 analyzer.py -r ../../results_of_simulation/no_shift_sim/results_max_max_clos"+format(i)+" it CSA > ../../results_of_simulation/data_no_shift_sim/data_max_max_clos_CSA"+format(i)+".txt"),
+#   os.system("python2 analyzer.py -r ../../results_of_simulation/no_shift_sim/results_max_max_clos"+format(i)+" it DSA > ../../results_of_simulation/data_no_shift_sim/data_max_max_clos_DSA"+format(i)+".txt"),
+    print i,
+    os.system("python2 analyzer.py -r ../../results_of_simulation/no_shift_sim/results_max_var_cons"+format(i)+" it CSA > ../../results_of_simulation/data_no_shift_sim/data_max_var_cons_CSA"+format(i)+".txt"),
+    os.system("python2 analyzer.py -r ../../results_of_simulation/no_shift_sim/results_max_var_cons"+format(i)+" it DSA > ../../results_of_simulation/data_no_shift_sim/data_max_var_cons_DSA"+format(i)+".txt"),
+    os.system("python2 analyzer.py -r ../../results_of_simulation/no_shift_sim/results_min_avg_bet"+format(i)+" it CSA > ../../results_of_simulation/data_no_shift_sim/data_min_avg_bet_CSA"+format(i)+".txt"),
+    os.system("python2 analyzer.py -r ../../results_of_simulation/no_shift_sim/results_min_avg_bet"+format(i)+" it DSA > ../../results_of_simulation/data_no_shift_sim/data_min_avg_bet_DSA"+format(i)+".txt"),
+    os.system("python2 analyzer.py -r ../../results_of_simulation/no_shift_sim/results_min_avg_clust"+format(i)+" it CSA > ../../results_of_simulation/data_no_shift_sim/data_min_avg_clust_CSA"+format(i)+".txt"),
+    os.system("python2 analyzer.py -r ../../results_of_simulation/no_shift_sim/results_min_avg_clust"+format(i)+" it DSA > ../../results_of_simulation/data_no_shift_sim/data_min_avg_clust_DSA"+format(i)+".txt")
+
+
+
