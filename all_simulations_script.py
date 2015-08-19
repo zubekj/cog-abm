8610a45bb7d9f280b5f7e629ba8d60e11e438054
julekb <julek.baranski@gmail.com>
2015-08-19 13:46:40 +0200

    Script upload. New shift simulations added.

--- all_simulations_script.py
+++ all_simulations_script.py
@@ -15,11 +15,11 @@ networks = {"max_avg_bet", "max_avg_clust", "max_max_bet", "max_max_clos", "max_
 for i in xrange(20):
     
     for network in networks:
-        os.system("python cog_simulations/steels/steels_main.py -s /examples/simulations/shift_simulations/simulation_" + network + "_to_clique.json -r  /results_of_simulation/shift_sim_results/results_" + network + "_to_clique" + format(i))
+        os.system("python cog_simulations/steels/steels_main.py -s examples/simulations/shift_simulations/simulation_" + network + "_to_clique.json -r  results_of_simulation/shift_sim_results/results_" + network + "_to_clique" + format(i))
     
-    os.system("python cog_simulations/steels/steels_main.py -s /examples/simulations/shift_simulations/simulation_line_to_clique.json -r  /results_of_simulation/shift_sim_results/results_line_to_clique" + format(i))
-    os.system("python cog_simulations/steels/steels_main.py -s /examples/simulations/shift_simulations/simulation_hub_to_clique.json -r  /results_of_simulation/shift_sim_results/results_hub_to_clique" + format(i))
-    os.system("python cog_simulations/steels/steels_main.py -s /examples/simulations/shift_simulations/simulation_ring_to_clique.json -r  /results_of_simulation/shift_sim_results/results_ring_to_clique" + format(i))
+    os.system("python cog_simulations/steels/steels_main.py -s /../../examples/simulations/shift_simulations/simulation_line_to_clique.json -r  /../../results_of_simulation/shift_sim_results/results_line_to_clique" + format(i))
+    os.system("python cog_simulations/steels/steels_main.py -s /../../examples/simulations/shift_simulations/simulation_hub_to_clique.json -r  /../../results_of_simulation/shift_sim_results/results_hub_to_clique" + format(i))
+    os.system("python cog_simulations/steels/steels_main.py -s /../../examples/simulations/shift_simulations/simulation_ring_to_clique.json -r  /../../results_of_simulation/shift_sim_results/results_ring_to_clique" + format(i))
 
 
 # analyzer
@@ -28,11 +28,11 @@ results = {"CSA", "DSA", "CLA", "DG_CLA"}
 for i in xrange(20):
     for result in results:
         for network in networks:
-            os.system("python cog_simulations/steels/analyzer.py -r results_of_simulation/shift_sim_results/results_" + network + "_to_clique" + format(i) + " it " + result + " > data_" + network + "_to_clique_" + result + "_" + format(i))
+            os.system("python cog_simulations/steels/analyzer.py -r /../../results_of_simulation/shift_sim_results/results_" + network + "_to_clique" + format(i) + " it " + result + " > data_" + network + "_to_clique_" + result + "_" + format(i))
 
-        os.system("python cog_simulations/steels/analyzer.py -r results_of_simulation/shift_sim_results/results_line_to_clique" + format(i) + " it " + result + " > data_line_to_clique_" + result + "_" + format(i))
-        os.system("python cog_simulations/steels/analyzer.py -r results_of_simulation/shift_sim_results/results_hub_to_clique" + format(i) + " it " + result + " > data_hub_to_clique_" + result + "_" + format(i))
-        os.system("python cog_simulations/steels/analyzer.py -r results_of_simulation/shift_sim_results/results_hub_to_clique" + format(i) + " it " + result + " > data_hub_to_clique_" + result + "_" + format(i))
+        os.system("python cog_simulations/steels/analyzer.py -r /../..results_of_simulation/shift_sim_results/results_line_to_clique" + format(i) + " it " + result + " > data_line_to_clique_" + result + "_" + format(i))
+        os.system("python cog_simulations/steels/analyzer.py -r /../..results_of_simulation/shift_sim_results/results_hub_to_clique" + format(i) + " it " + result + " > data_hub_to_clique_" + result + "_" + format(i))
+        os.system("python cog_simulations/steels/analyzer.py -r /../..results_of_simulation/shift_sim_results/results_hub_to_clique" + format(i) + " it " + result + " > data_hub_to_clique_" + result + "_" + format(i))
 
 
 # pandas
@@ -44,11 +44,23 @@ for network in networks:
     for result in results:
         stat[-1].append([])
         for i in xrange(20):
-            stat[-1][-1].append(pd.read_csv("data_" + network + "-" + result + format(i)), delim_whitespace=True, header=None, index_col=0)
+            stat[-1][-1].append(pd.read_csv("data_" + network + "_" + result + format(i)), delim_whitespace=True, header=None, index_col=0)
 # line, hub, ring
-    stat.append([])
-    stat.append([])
-    stat.append([])
+stat.append([])
+for result in results:
+    stat[-1].append([])
+    for i in xrange(20):
+        stat[-1][-1].append(pd.read_csv("data_line_" + result + format(i)), delim_whitespace=True, header=None, index_col=0)
+stat.append([])
+for result in results:
+    stat[-1].append([])
+    for i in xrange(20):
+        stat[-1][-1].append(pd.read_csv("data_hub_" + result + format(i)), delim_whitespace=True, header=None, index_col=0)
+stat.append([])
+for result in results:
+    stat[-1].append([])
+    for i in xrange(20):
+        stat[-1][-1].append(pd.read_csv("data_ring_" + result + format(i)), delim_whitespace=True, header=None, index_col=0)
 
 
 
