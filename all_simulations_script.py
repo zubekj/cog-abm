71c80413faff18be014095ce324706b540b1e3a4
julekb <julek.baranski@gmail.com>
2015-08-21 15:17:17 +0200

    Scripts updates

--- all_simulations_script.py
+++ all_simulations_script.py
@@ -25,7 +25,7 @@ results = {"CSA", "DSA", "CLA", "DG_CLA"}
 for i in xrange(20):
     for result in results:
         for network in networks+networks2:
-            os.system("python cog_simulations/steels/analyzer.py -r results_of_simulation/shift_sim_results/results_{0}_to_clique{1} it {2} > shift_sim_data/data_{0}_to_clique_{2}_{1}".format(network, i, result))
+            os.system("python cog_simulations/steels/analyzer.py -r results_of_simulation/shift_sim_results/results_{0}_to_clique{1} it {2} > shift_sim_data/data_{0}_to_clique_{2}{1}".format(network, i, result))
 
 
 # pandas
@@ -43,7 +43,7 @@ for result in results:
         mean = pd.DataFrame(index=index)
         var = pd.DataFrame(index=index)
         for i in xrange(20):
-            data_sim = pd.DataFrame(pd.read_csv("shift_sim_data/data_{0}_{1}{2}".format(network, result, i)), delim_whitespace=True, header=None, index_col=0)  ## czy tutaj?
+            data_sim = pd.DataFrame(pd.read_csv("shift_sim_data/data_{0}_to_clique_{1}{2}".format(network, result, i)), delim_whitespace=True, header=None, index_col=0)
             mean[i] = (data_sim.mean(1))
             var[i] = (data_sim.var(1))
         data[result + "_mean"][network] = mean.mean(1)
