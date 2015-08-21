f8586db478167957bacb8eb1a197e36ad5370d30
julekb <julek.baranski@gmail.com>
2015-08-21 10:08:02 +0200

    Script update.

--- all_simulations_script.py
+++ all_simulations_script.py
@@ -1,3 +1,7 @@
+def fun(networks):
+
+
+
 # -*- coding: utf-8 -*-
 # /COG_SIM
 
@@ -32,51 +36,30 @@ for i in xrange(20):
             os.system("python cog_simulations/steels/analyzer.py -r results_of_simulation/shift_sim_results/results" + network2 + "_to_clique" + format(i) + " it " + result + " > data_" + network2 + "_to_clique_" + result + "_" + format(i))
 
 # pandas
-# data [network][result][iter][DataFrame]
-# data[i][j][0] - mean and variance statistics of particular network, result in 20 iterations
 # czy wywalamy co drugi wiersz?
-index=range(2010)[::10]     # jak zrobić listę z krokiem 10?
-data = []
-mean_index = []
-var_index = []
+index = range(0, 2010, 10)
+columns = []
 
-for network in networks:
-    data.append([])
-    for result in results:
-        data[-1].append([])
-        data[-1][-1].append(pd.DataFrame(index=index))#, index_col=[mean_0, var_0,mean_1, var_1, mean_2, var_2, mean_3, var_3, mean_4, var_4, mean_5, var_5, mean_6, var_6, mean_7, var_7, mean_8, var_8, mean_9, var_9, mean_10, var_10, mean_11, var_11, mean_12, var_12, mean_13, var_13, mean_14, var_14, mean_15, var_15, mean_16, var_16, mean_17, var_17, mean_18, var_18, mean_19, var_19])
-        for i in xrange(20):
-            data[-1][-1].append(pd.read_csv("data_" + network + "_" + result + format(i)), delim_whitespace=True, header=None, index_col=0)
-            data[-1][-1][0]["mean_" + result + network + "_" + format(i)] = data[-1][-1][-1].mean(1)  # mean of i-th simulation
-            mean_index.append("mean_" + result + network + "_" + format(i))
-            data[-1][-1][0]["var_" + result + network + "_" + format(i)] = data[-1][-1][-1].var(1)
-            var_index.append("var_" + result + network + "_" + format(i))
-
-# line, hub, ring
-for network2 in networks2:
-    data.append([])
-    for result in results:
-        data[-1].append([])
-        data[-1][-1].append(pd.DataFrame(index=index))
-        for i in xrange(20):
-            data[-1][-1].append(pd.read_csv("data_" + network2 + "_" + result + format(i)), delim_whitespace=True, header=None, index_col=0)
-            data[-1][-1][0]["mean_" + result + network2 + "_" + format(i)] = data[-1][-1][-1].mean(1)  # mean of i-th simulation
-            data[-1][-1][0]["var_" + result + network2 + "_" + format(i)] = data[-1][-1][-1].var(1)
-
-stats = pd.DataFrame(index=index) # final statistics with mean and variance
-i = 0   #networks count
-j = 0   #resuts count
 for result in results:
-    for network in networks:
-        stats[result + network + "mean"] = data[i][j][0][mean_index].mean(1)
-        stats[result + network + "var"] = data[i][j][0][var_index].mean(1)
-        i += 1
-    for network2 in networks2:
-        i += 1
-        stats[result + network + "mean"] = data[i][j][0][mean_index].mean(1)
-        stats[result + network + "var"] = data[i][j][0][var_index].mean(1)
-    j += 1
-    i = 0
+    columns.append(result + "_mean")
+    columns.append(result + "_var")
+data = pd.DataFrame(index = networks + networks2, columns = columns)
+
+for result in results:
+    for network in networks+networks2:
+        data_sim = []
+        mean = pd.DataFrame(index=index)
+        var = pd.DataFrame(index=index)
+        for i in xrange(20):
+            data_sim.append(pd.read_csv("data_" + network + "_" + result + format(i)), delim_whitespace=True, header=None, index_col=0)
+            mean[i] = (data_sim[-1].mean(1))
+            var[i] = (data_sim[-1].var(1))
+        data[result + "_mean"][network] = mean.mean(1)
+        data[result + "_var"][network] = var.mean(1)
+
+
+
+
 
 # return stats
 
