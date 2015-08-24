93230afb7d2edb920741bf17dfece6c69b950305
przemekw <pwroblewski@student.uw.edu.pl>
2015-08-24 09:43:09 +0200

    Added sphinx comments to fitness.

--- cog_classification/core/fitness.py
+++ cog_classification/core/fitness.py
@@ -2,47 +2,83 @@ from collections import deque
 
 
 class Fitness:
+    """
+    Accumulates information about agent fitness.
+    """
 
     def __init__(self):
         pass
 
     def get_measure(self):
+        """
+        Exports information about agent fitness.
+        """
         pass
 
     def update(self, value):
+        """
+        Adds new information about agent fitness.
+
+        :param value: The new information about agent fitness.
+        """
         pass
 
 
 class StandardFitness(Fitness):
+    """
+    Monitors total win rate of agent.
+    """
 
     def __init__(self):
         self.success = 0
         self.all = 0
 
     def get_measure(self):
+        """
+        :return: Total win rate of agent.
+        :rtype: float
+        """
+
         if self.all == 0:
             return 0
         else:
             return float(self.success) / self.all
 
     def update(self, value):
+        """
+        :param value: The information about success or loss of agent.
+        :type value: bool
+        """
         self.all += 1
         self.success += value
 
 
 class CurrentFitness(Fitness):
+    """
+    Monitors current win rate of agent.
+
+    :param long scope: On how many last games the win rate will be calculated?
+    """
 
     def __init__(self, scope=50):
         self.scope = scope
         self.memories = deque([])
 
     def get_measure(self):
+        """
+        :return: Current win rate of agent.
+        :rtype: float
+        """
         if len(self.memories) < 1:
             return 0
         else:
             return float(sum(self.memories)) / len(self.memories)
 
     def update(self, value):
+        """
+        :param value: The information about success or loss of agent.
+        :type value: bool
+        """
         while len(self.memories) >= self.scope:
             self.memories.pop()
         self.memories.append(value)
