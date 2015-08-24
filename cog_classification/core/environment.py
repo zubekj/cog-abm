5f34ded50f867eec3e09454d8b80234fbcc93738
przemekw <pwroblewski@student.uw.edu.pl>
2015-08-24 09:33:51 +0200

    Added sphinx comments to environment.

--- cog_classification/core/environment.py
+++ cog_classification/core/environment.py
@@ -7,15 +7,20 @@ import random
 class Environment:
     """
     Stores samples and their classes.
+
+    :param list samples: List of samples. Not empty.
+    :param list classes: List of samples' classes. Not empty.
+    :param distance: distance function between two samples.
+    :type distance: function(sample, sample)
+
+    Samples and classes should have the same length.
     """
 
     def __init__(self, samples, classes, distance=None):
-        """
-        Args:
-            samples (list): list of samples.
-            classes (list): list of samples' classes.
-            distance (function): distance function between two samples.
-        """
+        assert samples is not None and classes is not None
+        assert not samples == [] and not classes == []
+        assert len(samples) == len(classes)
+
         self.samples = samples
         self.classes = classes
 
@@ -26,7 +31,15 @@ class Environment:
 
     @staticmethod
     def standard_distance(sample1, sample2):
-        """ Calculates standard distance between two samples of numerical values. """
+        """
+        Calculates standard distance between two samples of numerical values.
+
+        :param iterable sample1: First sample.
+        :param iterable sample2: Second sample.
+
+        :return: The calculated distance.
+        :rtype: float
+        """
 
         distance = 0
         for v1, v2 in izip(sample1, sample2):
@@ -36,24 +49,30 @@ class Environment:
 
     def get_all(self):
         """
-        Returns:
-            ((list, list)) list of samples and list of classes.
+        :returns: * List of all samples *(list)*
+            * List of all classes *(list)*
         """
         return self.samples, self.classes
-
-    def get_all_classes(self):
-        return self.classes
-
-    def get_all_samples(self):
-        return self.samples
 
     def get_class(self, index):
         """
         Returns class of sample with given index.
+
+        :param long index: The index of the sample whose class is needed.
+
+        :return: Class of sample with given index.
+        :rtype: numpy array
         """
         return np.array(self.classes[index])
 
     def get_random_sample(self):
+        """
+        Return a random sample from all samples.
+
+        :returns: * Index of drawn sample *(long)*
+            * Drawn sample *(list)*
+            * Class of drawn sample
+        """
         index = random.randrange(len(self.samples))
         sample = self.get_sample(index)
         sample_class = self.get_class(index)
@@ -62,17 +81,21 @@ class Environment:
     def get_sample(self, index):
         """
         Returns sample with specified index.
+
+        :param long index: Specified index.
+
+        :return: Sample associated with given index.
+        :rtype: numpy array
         """
         return np.array(self.samples[index])
 
     def get_samples(self, indexes):
         """
         Returns list of samples with given indexes.
+
+        :param iterable indexes: Specified indexes.
+
+        :return: Samples associated with given indexes.
+        :rtype: numpy array
         """
         return np.array([self.samples[index] for index in indexes])
-
-    def set_distance(self, distance):
-        """"
-        Change environment distance function.
-        """
-        self.distance = distance
