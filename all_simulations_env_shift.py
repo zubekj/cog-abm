b01393bc913ea73d3be0869405e7cfb27e03ef76
Julian Zubek <zubekj@gmail.com>
2015-09-14 13:44:11 +0200

    Code style

--- all_simulations_env_shift.py
+++ all_simulations_env_shift.py
@@ -4,7 +4,9 @@ import os
 from multiprocessing import Pool
 import pandas as pd # must be python2
 
-N_PROC = 4
+N_PROC = 2
+
+
 
 networks = ["max_avg_bet", "max_avg_clust", "max_max_bet", "max_max_clos",
             "max_var_cons", "min_avg_bet", "min_avg_clust", "min_max_clos"]
@@ -62,13 +64,13 @@ for result in results:
         data[result + "_var"][network] = var.mean(1)
 
 for network in networks+networks2:
-    data2 = pd.read_csv("results_of_simulation/env/shift_sim_data/data_env_training{0}cc_0".format(network), delim_whitespace=True, header=None, index_col=0)
-    for i in xrange(19):
-        data2 = data2 + pd.read_csv("results_of_simulation/env/shift_sim_data/data_env_training{0}cc_{1}".format(network, i+1), delim_whitespace=True, header=None, index_col=0)
-    data2 = data/20
+    data2 = pd.concat([pd.read_csv("results_of_simulation/env/shift_sim_data/data_env_training{0}cc_{1}".format(network, i), delim_whitespace=True, header=None, index_col=0)
+                       for i in xrange(20)], axis=2)
+    data2 = data2.mean(axis=2)
 
 
 with open("env_shift_sim_results.csv", 'w') as f:
-    data.to_csv(g)
-with open("env_shift_sim_cc_extended.csv", 'w') as g:
     data.to_csv(f)
+
+with open("env_shift_sim_cc_extended.csv", 'w') as f:
+    data2.to_csv(f)
