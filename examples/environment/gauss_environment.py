528f96414b4657a186a25e4bb4fc5bb83361c6d5
julekb <julek.baranski@gmail.com>
2015-10-29 12:39:53 +0100

    working script for creating a randomized munsell palette

--- examples/environment/gauss_environment.py
+++ examples/environment/gauss_environment.py
@@ -3,46 +3,38 @@ from scipy.spatial import distance
 import json
 
 STIMULI_NUM = 600
-STIM = (1,1,1)
+STIM = (47,7,39)
 COV = np.array([[1, 0, 0],
                 [0, 1, 0],
                 [0, 0, 1]])
 STIMULI_FILE = '1269_munsell_chips.json'
-DISTANCE = 50
+DIST = 10.433935691395032
 
 
-def LAB_dist(stim_1, stim_2):
-    return distance.euclidean(stim_1, stim_2)+11
+def fun(stimuli_file=STIMULI_FILE):
+    with open(stimuli_file, 'r') as g:
+        stimuli = json.load(g)
+    chips = [i.values() for i in stimuli['stimuli']]
+    min_dist = 0
+    for chip in chips:
+        dist = distance.cdist([chip], [i for i in chips if i!=chip], 'euclidean')
+        if (min_dist <= np.min(dist)):
+            min_dist = np.min(dist)
+    return min_dist
 
 
 def gauss_divide(output_name, stim=STIM, stimuli_num=STIMULI_NUM, stimuli_file=STIMULI_FILE, cov=COV):
     with open(stimuli_file, 'r') as g:
         stimuli = json.load(g)
     chips = [i.values() for i in stimuli['stimuli']]
-    dists = distance.pdist(chips, metric='euclidean')
-    # print np.max(dists)
-    # min=100
-    # for i in dists:
-    #     if (i != 0 & i < min):
-    #         min = i
-    #
-    # print i
-
-
-
-    for i in xrange(stimuli_num):
+    random_chips = []
+    i = 0
+    while ( i < stimuli_num):
         x = np.random.multivariate_normal(stim, cov)
+        if np.min(distance.cdist([x], chips, 'euclidean')) <= DIST:
+            random_chips.append({"a": x[0], "L": x[1], "b": x[2]})
+            i = i + 1
 
-       # distances = distance.cdist([x], chips)
-
-
-
-       # random_chips.append(chips[distances.argmin()])
-
-        if LAB_dist(stim, x) > 50:
-            i -= 1
-        else:
-            chips.append({"a": x[0], "L": x[1], "b": x[2]})
     with open(output_name+".json", 'w') as f:
-        json.dump({"stimuli": chips, "type": "CIELab"}, f)
+        json.dump({"stimuli": random_chips, "type": "CIELab"}, f)
 
