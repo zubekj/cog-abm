8edf4dcd593dcaa6c62a004b3e964008a56d9d5e
Julian Zubek <zubekj@gmail.com>
2016-04-13 18:44:32 +0200

    Updated plots.

--- manuscript_plots/simulation_results/generate_plots.py
+++ manuscript_plots/simulation_results/generate_plots.py
@@ -31,7 +31,7 @@ with open("top_shift_mason.tikz", "w") as f:
             "every tick/.style={white},"\
             "legend style={\n"\
             "at={(1.05, 1.0)}, anchor=north west},"\
-            "ylabel=Mean CS\\textsubscript\{L\}]\n")
+            "ylabel=Mean CS\\textsubscript{L}]\n")
 
     for g in ["clique"]:
             f.write("\\addplot +[mark=none, dotted, thick] table[col sep=comma, x=Iteration, "\
@@ -66,7 +66,7 @@ with open("top_shift_mason.tikz", "w") as f:
             "every tick/.style={white},"\
             "legend style={\n"\
             "at={(0, -0.2)}, anchor=north west},"\
-            "ylabel=Mean CS\\textsubscript\{G\}]\n")
+            "ylabel=Mean CS\\textsubscript{G}]\n")
 
     for g in ["clique"]:
             f.write("\\addplot +[mark=none, dotted, thick] table[col sep=comma, x=Iteration, "\
@@ -103,7 +103,7 @@ with open("top_shift_star.tikz", "w") as f:
             "every tick/.style={white},"\
             "legend style={\n"\
             "at={(1.05, 1.0)}, anchor=north west},"\
-            "ylabel=Mean CS\\textsubscript\{L\}]\n")
+            "ylabel=Mean CS\\textsubscript{L}]\n")
 
     for g in ["clique"]:
             f.write("\\addplot +[mark=none, dotted, thick] table[col sep=comma, x=Iteration, "\
@@ -137,7 +137,7 @@ with open("top_shift_star.tikz", "w") as f:
             "every tick/.style={white},"\
             "legend style={\n"\
             "at={(0, -0.2)}, anchor=north west},"\
-            "ylabel=Mean CS\\textsubscript\{G\}]\n")
+            "ylabel=Mean CS\\textsubscript{G}]\n")
 
     for g in ["clique"]:
             f.write("\\addplot +[mark=none, dotted, thick] table[col sep=comma, x=Iteration, "\
@@ -168,7 +168,7 @@ with open("env_shift.tikz", "w") as f:
             "every tick/.style={white},"\
             "legend style={\n"\
             "at={(1.05, 1.0)}, anchor=north west},"\
-            "ylabel=Mean CS\\textsubscript\{L\}]\n")
+            "ylabel=Mean CS\\textsubscript{L}]\n")
 
     for g in ["clique"]:
             f.write("\\addplot +[mark=none, dotted, thick] table[col sep=comma, x=Iteration, "\
@@ -209,7 +209,7 @@ with open("env_shift.tikz", "w") as f:
             "every tick/.style={white},"\
             "legend style={\n"\
             "at={(0, -0.2)}, anchor=north west},"\
-            "ylabel=Mean CS\\textsubscript\{G\}]\n")
+            "ylabel=Mean CS\\textsubscript{G}]\n")
 
     for g in ["clique"]:
             f.write("\\addplot +[mark=none, dotted, thick] table[col sep=comma, x=Iteration, "\
@@ -246,35 +246,30 @@ with open("env_shift_gauss.tikz", "w") as f:
             "every tick/.style={white},"\
             "legend style={\n"\
             "at={(1.05, 1.0)}, anchor=north west},"\
-            "ylabel=Mean CS\\textsubscript\{A\}]\n")
+            "ylabel=Mean CS\\textsubscript{A}]\n")
 
     for g in ["clique"]:
             f.write("\\addplot +[mark=none, dotted, thick] table[col sep=comma, x=Iteration, "\
                     "y=CSA_mean] "\
                     "{{simulation_results/env_shift_gauss_{0}.csv}};\n".format(g))
-            label = g.replace("_", " ")
-            f.write("\\addlegendentry{{{0}}}\n".format(label))
 
     for g in ["max_avg_bet", "max_avg_clust", "max_max_bet", "min_max_clos", "max_var_cons", "min_avg_bet", "min_avg_clust", "max_max_clos"]:
-            f.write("\\addplot +[mark=none, dashed, thick] table[col sep=comma, x=Iteration, "\
-                    "y=CSA_mean] "\
-                    "{{simulation_results/env_shift_gauss_{0}.csv}};\n".format(g))
-            label = g.replace("_", " ")
-            f.write("\\addlegendentry{{{0}}}\n".format(label))
+            if g in ["max_avg_bet", "min_avg_bet"]:
+                f.write("\\addplot +[mark=none, dashed, thick] table[col sep=comma, x=Iteration, "\
+                        "y=CSA_mean] "\
+                        "{{simulation_results/env_shift_gauss_{0}.csv}};\n".format(g))
+            else:
+                f.write("\\addplot +[draw=none] table[col sep=comma, x=Iteration, "\
+                        "y=CSA_mean] "\
+                        "{{simulation_results/env_shift_gauss_{0}.csv}};\n".format(g))
+
 
     for g in ["hub", "hub_hearer", "hub_speaker"]:
             f.write("\\addplot +[mark=none, solid, thick] table[col sep=comma, x=Iteration, "\
                     "y=CSA_mean] "\
                     "{{simulation_results/env_shift_gauss_{0}.csv}};\n".format(g))
-            label = g.replace("_", " ")
-            if label == "hub":
-                label = "star"
-            elif label == "hub hearer":
-                label = "star speaker"
-            elif label == "hub speaker":
-                label = "star hearer"
-            f.write("\\addlegendentry{{{0}}}\n".format(label))
 
+    f.write("\\legend{clique, max avg bet, , , , , min avg bet, , , star, star speaker, star hearer}")
     f.write("\\draw[dashed] (axis cs:9999,-0.1) -- (axis cs:9999,1);\n")
     f.write("\\end{axis}\n")
 
@@ -287,7 +282,7 @@ with open("env_shift_gauss.tikz", "w") as f:
             "every tick/.style={white},"\
             "legend style={\n"\
             "at={(0, -0.2)}, anchor=north west},"\
-            "ylabel=Mean CS\\textsubscript\{B\}]\n")
+            "ylabel=Mean CS\\textsubscript{B}]\n")
 
     for g in ["clique"]:
             f.write("\\addplot +[mark=none, dotted, thick] table[col sep=comma, x=Iteration, "\
@@ -296,10 +291,16 @@ with open("env_shift_gauss.tikz", "w") as f:
             label = g.replace("_", " ")
 
     for g in ["max_avg_bet", "max_avg_clust", "max_max_bet", "min_max_clos", "max_var_cons", "min_avg_bet", "min_avg_clust", "max_max_clos"]:
+        if g in ["max_avg_bet", "min_avg_bet"]:
             f.write("\\addplot +[mark=none, dashed, thick] table[col sep=comma, x=Iteration, "\
                     "y=CLA_mean] "\
                     "{{simulation_results/env_shift_gauss_{0}.csv}};\n".format(g))
             label = g.replace("_", " ")
+        else:
+            f.write("\\addplot +[draw=none] table[col sep=comma, x=Iteration, "\
+                    "y=CLA_mean] "\
+                    "{{simulation_results/env_shift_gauss_{0}.csv}};\n".format(g))
+
 
     for g in ["hub", "hub_hearer", "hub_speaker"]:
             f.write("\\addplot +[mark=none, solid, thick] table[col sep=comma, x=Iteration, "\
