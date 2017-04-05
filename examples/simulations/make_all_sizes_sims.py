d01747ad0e92ce8c7390db1d0a42648252a046e8
mdenkiewicz <michal.denkiewicz@gmail.com>
2017-04-05 14:06:13 +0200

    Add variable number of iterations

--- examples/simulations/make_all_sizes_sims.py
+++ examples/simulations/make_all_sizes_sims.py
@@ -4,20 +4,22 @@ import re
 import logging
 
 
-BASE_DIR = 'base_sims'
-OUT_DIR = '.'
 SIZES = [8, 12, 24, 32, 48]  # No 16!
+DUMP_PER_8_NODES = 25
+ITERS_PER_8_NODES = DUMP_PER_8_NODES * 200
 
 
 logging.basicConfig(level=logging.INFO)
 
-base_sim_files = [
-    (name, path) for name, path in
-    ((name, os.path.join(BASE_DIR, name)) for name in os.listdir(BASE_DIR))
-    if os.path.isfile(path) and re.match(r'^simulation_\w+\.json$', name)
-]
 
-logging.info("Found %d base sim files.", len(base_sim_files))
+def main(base_dir, out_dir):
+    base_sim_files = [
+        (name, path) for name, path in
+        ((name, os.path.join(base_dir, name)) for name in os.listdir(base_dir))
+        if os.path.isfile(path) and re.match(r'^simulation_\w+\.json$', name)
+    ]
+    logging.info("Found %d base sim files.", len(base_sim_files))
+    process(base_sim_files, out_dir)
 
 
 def mk_source(source, n):
@@ -27,26 +29,31 @@ def mk_source(source, n):
     return unicode(fn)
 
 
-def main():
+def process(base_sim_files, out_dir):
     for name, in_path in base_sim_files:
         logging.info("Processing file %s", in_path)
         for size in SIZES:
             with open(in_path)as f_in:
                 sim = json.load(f_in)  # restore state
             sim['num_agents'] = size
+            epoch_size = (size // 8) * ITERS_PER_8_NODES
+            sim['num_iter'] = epoch_size * 2
+            sim['dump_freq'] = (size // 8) * DUMP_PER_8_NODES
             if 'interactions_sets' in sim:
                 for _, lst in sim['interactions_sets'].items():
                     for elem in lst:
                         if elem['network']['type'] == "Source":
                             elem['network']['source'] = mk_source(elem['network']['source'], size)
-                        # elem['environment']['source'] = '../' + elem['environment']['source']
+                sim['interactions_sets'][str(epoch_size)] = sim['interactions_sets']['10000']
+                del sim['interactions_sets']['10000']
             network = re.match(r'simulation_(\w+)\.json', name).group(1)
             out_name = 'simulation_N{:02d}_{}.json'.format(size, network)
-            out_path = os.path.join(OUT_DIR, out_name)
+            out_path = os.path.join(out_dir, out_name)
             logging.info("Writing file %s", out_path)
             with open(out_path, 'w') as out_f:
                 json.dump(sim, out_f, indent=4)
 
 
 if __name__ == "__main__":
-    main()
+    main('ext_top_shift/base_sims', 'ext_top_shift')
+    main('ext_env_shift/base_sims', 'ext_env_shift')
