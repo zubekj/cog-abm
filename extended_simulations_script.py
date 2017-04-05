e1951ac4670b12122ebebddf37915ab9e88daaa4
mdenkiewicz <michal.denkiewicz@gmail.com>
2017-04-05 15:47:17 +0200

    Use concurrency.futures

--- extended_simulations_script.py
+++ extended_simulations_script.py
@@ -2,7 +2,7 @@
 # /COG_SIM
 import os
 import re
-from multiprocessing import Pool
+import concurrency.futures as cf
 import pandas as pd  # must be python2
 
 
@@ -21,17 +21,23 @@ def run_simulations(sim_base_dir, res_base_dir, n_sim, n_proc):
     for fn in simulation_files:
         print(fn)
 
-    pool = Pool(processes=N_PROC)
+    future_to_desc = {}
+    with cf.ProcessPoolExecutor(max_workers=n_proc) as ex:
+        # simulations
+        for i in range(N_SIM):
+            for sim_fname in simulation_files:
+                res_fname = os.path.join(res_base_dir, "{0}_{1}.csv".format(os.path.basename(sim_fname), i))
+                if not os.path.isfile(res_fname):
+                    cmd = "python2 cog_simulations/steels/steels_main.py -s {0} -r {1}".format(sim_fname, res_fname)
+                    f = ex.submit(os.system, cmd)
+                    future_to_desc[f] = sim_fname
+                else:
+                    print(res_fname + ' already exists, skipping.')
 
-    # simulations
-    for i in range(N_SIM):
-        for sim_fname in simulation_files:
-            res_fname = os.path.join(res_base_dir, "{0}_{1}.csv".format(os.path.basename(sim_fname), i))
-            if not os.path.isfile(res_fname):
-                pool.apply_async(os.system, ["python2 cog_simulations/steels/steels_main.py -s {0} -r {1}".format(sim_fname, res_fname)])
-
-    pool.close()
-    pool.join()
+        n_completed = 0
+        for f in cf.as_completed(future_to_desc):
+            n_completed += 1
+            print("Completed {} ({}/{})".format(future_to_desc[f], n_completed, len(future_to_desc)))
 
     print("Simulations done.")
 
@@ -79,7 +85,7 @@ def merge_results(res_base_dir, results_file):
 def run_sim(name):
     sim_base_dir = 'examples/simulations/%s' % name
     res_base_dir = 'results_of_simulation/%s' % name
-    results_file = '%s_results.csv' % name
+    results_file = '%s_results_vi.csv' % name
     run_simulations(sim_base_dir, res_base_dir, N_SIM, N_PROC)
     merge_results(res_base_dir, results_file)
 
